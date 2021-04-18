525bd4dd233dc118ec345b808179fe8aeec5f6ef
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-04-18 23:32:25 +0530

    feat: regex pattern and require courseAbbr so invalid can trigger the red

--- src/CourseAbbr.svelte
+++ src/CourseAbbr.svelte
@@ -4,5 +4,5 @@
 </script>
 <div class="flex-parent today-input-section">
 	<h4>Course/Paper Abbrev</h4>
-	<input value={courseAbbr} placeholder="Enter the course abbreviation" on:input={inputHandler}>
+	<input value={courseAbbr} pattern=".+" placeholder="Enter the course abbreviation" on:input={inputHandler} required>
 </div>
