96cf9a271e65f5fa6c35151f5539f1e49c32dfec
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-04-19 13:39:30 +0530

    feat: copied prompt

--- src/Filename.svelte
+++ src/Filename.svelte
@@ -2,6 +2,7 @@
 	export let data
 	import copyTextToClipboard from "copy-to-clipboard"
 	let filenameField
+	let filenameGrid
 	
 	const FALSY_VALUES = ["false", false, undefined, "undefined", null, "null", 0, "0", "", " "]
 	const isFalsy = (val) => FALSY_VALUES.some( (falsy) => val === falsy )
@@ -9,13 +10,16 @@
 
 	$: copyButtonIsDisabled = someAreFalsy(data)
 	$: filename = `${data.regnum}_${ isFalsy(data.courseAbbr) ? "XYZ" : data.courseAbbr }_${data.pageCount}pgs`
+
 	const copyToClipboard = () => {
 		copyTextToClipboard(filename)
 		console.log(`"${filename}" has been copied to clipboard`)
+		filenameGrid.setAttribute("data-value", "Copied!")
+		setTimeout(()=>{filenameGrid.setAttribute("data-value", "")}, 3000)
 	}
 </script>
 
-<div class="grid-parent filename-grid">
+<div data-value="" bind:this={filenameGrid} class="grid-parent filename-grid">
 	<input type="text" value={filename} bind:this={filenameField} on:click={copyButtonIsDisabled || filenameField.select()} readonly>
 	<button disabled={copyButtonIsDisabled} alt="Click to copy" on:click="{copyToClipboard}">
 	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg>
@@ -23,6 +27,11 @@
 </div>
 
 <style>
+	div::after{
+		content: attr(data-value);
+		display: block;
+		transition: 5s;
+	}
 	button {
 		display: grid;
 		padding: 0.25ch;
